6706f67c4af4347c05f61fa54202181bb6ccf16c
peterclermy232 <peterclermy232@gmail.com>
2022-10-13 19:22:34 +0300

    project

---
+++ client/src/containers/search/ListResults.js
@@ -0,0 +1,86 @@
+import * as React from 'react'
+import {useEffect, useState}  from 'react'
+import '../../css/Form.css'
+import {
+  Grid,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@mui/material'
+import ListResult from '../../components/search/ListResult'
+import Loading from '../../components/Loading'
+
+const ListResults = ({
+  recommendationLists,
+  handleListSearch,
+  currentList,
+  handleFetchBook,
+  loading,
+}) => {
+  const [listValue, setListValue] = useState(
+    currentList ? currentList.id : ""
+  )
+  const [books, setBooks] = useState(
+    currentList ? currentList.list.items : ""
+  )
+
+  useEffect(() => {
+    setListValue(currentList ? currentList.id : "")
+    setBooks(currentList ? currentList.list.items : "")
+  }, [currentList])
+
+  const handleListChange = (e) => {
+    setListValue(e.target.value)
+    handleListSearch(e.target.value)
+  }
+  return (
+    <Grid item container flexDirection='column' wrap='nowrap' spacing={4}>
+      <Grid item>
+        <Typography component='h2' variant='h4' align='center'>
+          Search Books By Popular Lists
+        </Typography>
+      </Grid>
+      <Grid item xs={12} textAlign='center'>
+        <FormControl className='form-lists'>
+          <InputLabel id='list-recommendations'>Lists</InputLabel>
+          <Select
+            labelId='list-recommendations'
+            onChange={handleListChange}
+            value={listValue}>
+            {recommendationLists.map((rec) => (
+              <MenuItem value={rec.id}>{rec.name}</MenuItem>
+            ))}
+            
+          </Select>
+        </FormControl>
+      </Grid>
+
+      <Grid
+        container
+        item
+        spacing={5}
+        sx={{ pt: 6 }}
+        alignItems='center'
+        justifyContent='center'>
+        {loading ? (
+          <Loading />
+        ) : (
+          books &&
+          books
+            .sort((a, b) => a.rankingPosition - b.rankingPosition)
+            .map((book) => (
+              <ListResult
+                key={book.id}
+                book={book}
+                handleFetchBook={handleFetchBook}
+              />
+            ))
+        )}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default ListResults
